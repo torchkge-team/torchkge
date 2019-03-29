20d85377ce226c4de12c57f90582ba90dcfabb6c
armand33 <aboschin@pm.me>
2019-03-29 15:34:45 +0100

    Initial settings

---
+++ docs/reference.rst
@@ -0,0 +1,18 @@
+=========
+Reference
+=========
+
+A Nice Section
+--------------
+
+.. autoclass:: torchkge.MyClass1
+    :members:
+
+Another Nice Section
+--------------------
+
+.. autoclass:: torchkge.MyClass2
+    :members:
+
+.. autoclass:: torchkge.MyClass3
+    :members:
--- setup.py
+++ setup.py
@@ -26,8 +26,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
