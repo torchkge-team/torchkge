3d4fa1aad7dd723ec3971386e3b764d29f61ca5d
armand33 <aboschin@pm.me>
2019-04-01 14:41:08 +0200

    Added torchvision to requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'torch==1.0.1.post2']
+requirements = ['Click>=6.0', 'torch==1.0.1.post2', 'torchvision']
 
 setup_requirements = ['pytest-runner', ]
 
