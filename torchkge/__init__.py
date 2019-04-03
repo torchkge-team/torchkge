40988c4d1d8c67667623bf96798e6954aa4d9a8e
armand33 <aboschin@pm.me>
2019-04-03 11:10:28 +0200

    Bump version: 0.1.6 → 0.1.7

--- torchkge/__init__.py
+++ torchkge/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Armand Boschin"""
 __email__ = 'armand.boschin@telecom-paristech.fr'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 
 
 from .evaluation.Dissimilarities import l1_dissimilarity
