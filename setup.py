ddb53e22b3620f28bc8f761d6fcfc0a8b3d73ffb
armand33 <aboschin@pm.me>
2019-06-24 15:52:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['torch==1.1.0', 'torchvision', 'tqdm==4.31.1']
+requirements = ['torch==1.1.0post2', 'torchvision', 'tqdm==4.32.2']
 
 setup_requirements = []
 
