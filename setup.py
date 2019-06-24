266c4f4973595526823282e50b16cf704a4503ff
armand33 <aboschin@pm.me>
2019-06-24 22:04:04 +0200

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/torchkge-team/torchkge',
-    version='0.4.0',
+    version='0.5.0',
     zip_safe=False,
 )
--- torchkge/__init__.py
+++ torchkge/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Armand Boschin"""
 __email__ = 'aboschin@enst.fr'
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 
 from .evaluation.Dissimilarities import l1_dissimilarity
