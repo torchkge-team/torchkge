5e5401e476e06a9e70887b3a8840e60a3a4a2c69
armand33 <aboschin@pm.me>
2019-06-25 09:32:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     include_package_data=True,
     keywords='torchkge',
     name='torchkge',
-    packages=find_packages(include=['torchkge']),
+    packages=find_packages(),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
