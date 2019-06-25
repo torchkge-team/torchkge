2ab510f23d0a397ede7055e6045300513bd48e83
armand33 <aboschin@pm.me>
2019-06-25 09:33:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,11 +28,6 @@ setup(
         'Programming Language :: Python :: 3.7',
     ],
     description="Knowledge Graph embedding in Python and Pytorch.",
-    entry_points={
-        'console_scripts': [
-            'torchkge=torchkge.cli:main',
-        ],
-    },
     install_requires=requirements,
     license="BSD license",
     long_description=readme + '\n\n' + history,
