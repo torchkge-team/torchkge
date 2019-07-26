aac75faa46c72b695d07382cf3f666e5c8a35abf
armand33 <aboschin@pm.me>
2019-07-26 14:58:03 +0200

    Fix path to logo

--- docs/conf.py
+++ docs/conf.py
@@ -94,8 +94,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_logo = '../images/logo_torchkge.png'
-html_favicon = "../images/logo_torchkge.png"
+html_logo = '../images/logo_torchKGE.png'
+html_favicon = "../images/logo_torchKGE.png"
 html_theme_options = {
     'logo_only': True
 }
