8f2d9a4ea3204bd2bff4b72f04178246aa847d16
armand33 <aboschin@pm.me>
2019-10-21 18:13:08 +0200

    Bump version: 0.10.6 → 0.11.0

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/torchkge-team/torchkge',
-    version='0.10.6',
+    version='0.11.0',
     zip_safe=False,
 )
