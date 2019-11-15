93eb5b0b6adb65b67ef90a5223d94eddaf89a1eb
armand <aboschin@enst.fr>
2019-11-15 13:17:31 +0100

    Merge dev requirements into setup script

--- setup.py
+++ setup.py
@@ -14,6 +14,10 @@ setup_requirements = []
 
 test_requirements = []
 
+dev_requirements = ['torch==1.3.1', 'pip', 'bumpversion==0.5.3', 'sphinx==1.8.5', 'sphinx_rtd_theme==0.4.3',
+                    'numpydoc==0.9.1', 'wheel==0.33.6', 'watchdog==0.9.0', 'flake8==3.7.9', 'tox==3.14.0',
+                    'coverage==4.5.4', 'twine==2.0.0']
+
 setup(
     author="TorchKGE Developers",
     author_email='aboschin@enst.fr',
@@ -25,17 +29,18 @@ setup(
         'Programming Language :: Python :: 3.7',
     ],
     description="Knowledge Graph embedding in Python and PyTorch.",
-    install_requires=requirements,
     license="BSD license",
     long_description=readme,
     include_package_data=True,
     keywords='torchkge',
     name='torchkge',
-    packages=find_packages(),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
     url='https://github.com/torchkge-team/torchkge',
+    packages=find_packages(),
+    install_requires=requirements,
+    setup_requires=setup_requirements,
+    tests_require=test_requirements,
+    extra_require={'dev': dev_requirements},
+    test_suite='tests',
     version='0.11.2',
     zip_safe=False,
 )
