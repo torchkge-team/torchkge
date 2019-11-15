c25b05bfbdea8aec37c633071fcb0f5195eec7ea
armand <aboschin@enst.fr>
2019-11-15 13:05:09 +0100

    Removed history from readme

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('docs/history.rst') as history_file:
-    history = history_file.read()
-
 requirements = ['torch>=1.2.0', 'tqdm', 'pandas']
 
 setup_requirements = []
@@ -30,7 +27,7 @@ setup(
     description="Knowledge Graph embedding in Python and PyTorch.",
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='torchkge',
     name='torchkge',
