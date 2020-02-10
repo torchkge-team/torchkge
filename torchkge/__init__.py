094066faf1ea182c712955adfa5d164b8ce0f437
armand33 <aboschin@pm.me>
2020-02-10 15:50:47 +0100

    Added ConvKB model.

--- torchkge/__init__.py
+++ torchkge/__init__.py
@@ -10,6 +10,7 @@ from .data import KnowledgeGraph
 
 from .models import TransEModel, TransHModel, TransRModel, TransDModel
 from .models import RESCALModel, DistMultModel
+from .models import ConvKBModel
 
 from torchkge.utils import l1_dissimilarity, l2_dissimilarity
 from torchkge.utils import MarginLoss, LogisticLoss
