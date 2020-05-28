a61490e05b92828a844da09e56923b790190e6e6
armand <aboschin@pm.me>
2020-05-28 18:52:00 +0200

    Fixed import problem

--- torchkge/utils/__init__.py
+++ torchkge/utils/__init__.py
@@ -1,5 +1,8 @@
 from .data import DataLoader, get_data_home, clear_data_home
 
+from .datasets import load_fb15k, load_fb13, load_fb15k237, load_wn18, \
+    load_wn18rr, load_yago3_10, load_wikidatasets
+
 from .dissimilarities import l1_dissimilarity, l2_dissimilarity
 from .dissimilarities import l1_torus_dissimilarity, l2_torus_dissimilarity, \
     el2_torus_dissimilarity
@@ -8,7 +11,7 @@ from .losses import MarginLoss, LogisticLoss
 
 from .modelling import init_embedding, get_true_targets, load_embeddings
 
-from .operations import get_rank, get_mask, get_dictionaries, \
+from .operations import get_rank, get_mask, \
     get_bernoulli_probs
 
 from .training import Trainer, TrainDataLoader
