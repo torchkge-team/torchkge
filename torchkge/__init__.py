fe08ed1f26d3897fe0396b7534d1a745746fd359
armand <aboschin@pm.me>
2020-05-29 11:55:40 +0200

    Bump version: 0.16.2 → 0.16.3

--- torchkge/__init__.py
+++ torchkge/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Armand Boschin"""
 __email__ = 'aboschin@enst.fr'
-__version__ = '0.16.2'
+__version__ = '0.16.3'
 
 from torchkge.exceptions import NotYetEvaluatedError
 from torchkge.utils import MarginLoss, LogisticLoss
