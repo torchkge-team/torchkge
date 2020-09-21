43349fbc208051d8f42cf90ea46ef7589d4c7a84
armand <aboschin@pm.me>
2020-09-21 17:11:41 +0200

    Bump version: 0.16.13 → 0.16.14

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     setup_requires=setup_requirements,
     tests_require=test_requirements,
     test_suite='tests',
-    version='0.16.13',
+    version='0.16.14',
     zip_safe=False,
 )
