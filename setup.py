d7c901889666166bd07ddc042ab351c70bdd7757
armand <aboschin@pm.me>
2021-01-22 11:55:06 +0100

    Add support for python 3.8

--- setup.py
+++ setup.py
@@ -22,7 +22,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     description="Knowledge Graph embedding in Python and PyTorch.",
     license="BSD license",
