44ab0fd592b71ffb67c9017ad540a323bd35bc27
armand <aboschin@pm.me>
2022-02-24 18:50:48 +0100

    Update modeling.py

--- torchkge/utils/modeling.py
+++ torchkge/utils/modeling.py
@@ -50,7 +50,7 @@ def load_embeddings(model, dim, dataset, data_home=None):
     return state_dict
 
 
-def get_true_targets(dictionary, e_idx, r_idx, true_idx, i):
+def get_true_targets(dictionary, key1, key2, true_idx, i):
     """For a current index `i` of the batch, returns a tensor containing the
     indices of entities for which the triplet is an existing one (i.e. a true
     one under CWA).
@@ -60,10 +60,8 @@ def get_true_targets(dictionary, e_idx, r_idx, true_idx, i):
     dictionary: default dict
         Dictionary of keys (int, int) and values list of ints giving all
         possible entities for the (entity, relation) pair.
-    e_idx: torch.Tensor, shape: (batch_size), dtype: torch.long
-        Tensor containing the indices of entities.
-    r_idx: torch.Tensor, shape: (batch_size), dtype: torch.long
-        Tensor containing the indices of relations.
+    key1: torch.Tensor, shape: (batch_size), dtype: torch.long
+    key2: torch.Tensor, shape: (batch_size), dtype: torch.long
     true_idx: torch.Tensor, shape: (batch_size), dtype: torch.long
         Tensor containing the true entity for each sample.
     i: int
@@ -76,8 +74,29 @@ def get_true_targets(dictionary, e_idx, r_idx, true_idx, i):
         (e_idx[i], r_idx[i], true_target[any]) is a true fact.
 
     """
-    true_targets = dictionary[e_idx[i].item(), r_idx[i].item()].copy()
-    if len(true_targets) == 1:
+    try:
+        true_targets = dictionary[key1[i].item(), key2[i].item()].copy()
+        if true_idx is not None:
+            true_targets.remove(true_idx[i].item())
+            if len(true_targets) > 0:
+                return tensor(list(true_targets)).long()
+            else:
+                return None
+        else:
+            return tensor(list(true_targets)).long()
+    except KeyError:
         return None
-    true_targets.remove(true_idx[i].item())
-    return tensor(list(true_targets)).long()
+
+
+def filter_scores(scores, dictionary, key1, key2, true_idx):
+    # filter out the true negative samples by assigning - inf score.
+    b_size = scores.shape[0]
+    filt_scores = scores.clone()
+
+    for i in range(b_size):
+        true_targets = get_true_targets(dictionary, key1, key2, true_idx, i)
+        if true_targets is None:
+            continue
+        filt_scores[i][true_targets] = - float('Inf')
+
+    return filt_scores
