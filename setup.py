0f72c7415126f15464d6b2445db6a96b60b7e69d
armand <aboschin@pm.me>
2022-02-25 17:13:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-requirements = ['torch>=1.2.0', 'tqdm', 'pandas', 'numpy']
+requirements = ['torch>=1.2.0', 'tqdm', 'pandas', 'numpy==1.21.5']
 
 setup_requirements = ['pytest-runner']
 
