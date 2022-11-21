a4f2ce7d7d3f41fa16b4d51ad88455b1af23ce90
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-21 05:22:29 +0000

    Adding tarfile member sanitization to extractall()

--- torchkge/utils/modeling.py
+++ torchkge/utils/modeling.py
@@ -41,7 +41,29 @@ def load_embeddings(model, dim, dataset, data_home=None):
         urlretrieve("https://graphs.telecom-paris.fr/data/torchkge/models/{}_{}_{}.tar.gz".format(model, dataset, dim),
                     targz_file)
         with tarfile.open(targz_file, 'r') as tf:
-            tf.extractall(data_path)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tf, data_path)
         remove(targz_file)
 
     with open(pkl_file, 'rb') as f:
